3329c06beb69a5b18f5fc11b9d65c74618b0186b
Anna <anna.pedersen@student.education.wa.edu.au>
2022-12-21 13:18:49 +0800

    deleted bad stuff

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = '4788 Code Guide'
+copyright = '2022, 4788 Cant Control'
+author = 'Anna'
 
 release = '0.1'
 version = '0.1.0'
